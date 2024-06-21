3d7a8a6ebc9463841ee280f79022895589af8d1c
bootsie123 <bootsie227@gmail.com>
2024-06-21 22:46:43 +0000

    update(file): - Updated merge script - Updated generate workflow

--- scripts/merge.js
+++ scripts/merge.js
@@ -1,5 +1,7 @@
 import fs from "fs";
 
+console.log("Merging OpenAPI specs...");
+
 const openapi = JSON.parse(fs.readFileSync("./openapi.json"));
 
 openapi.info.title = "Iconik";
@@ -10,3 +12,19 @@ openapi.servers = [
 ];
 
 fs.writeFileSync("./openapi.json", JSON.stringify(openapi, null, 2));
+
+console.log("Specs merged!");
+
+const version = openapi?.info?.version;
+
+const packageJSON = JSON.parse(fs.readFileSync("./package.json"));
+
+if (version && version !== packageJSON.json) {
+  console.log(
+    `OpenAPI (${version}) and package.json (${packageJSON.version}) versions are mismatched!\nUpdating package.json version to ${version}...`
+  );
+
+  packageJSON.version = version;
+
+  fs.writeFileSync("./package.json", JSON.stringify(packageJSON, null, 2));
+}
