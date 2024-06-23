5402bd53851c49d42e66ef81499d00851daefe8e
bootsie123 <bootsie227@gmail.com>
2024-06-23 02:48:13 +0000

    update(file): - Added security schemes when merging - Added script to locally test openapi-generator - Removed ES6 support

--- scripts/merge.js
+++ scripts/merge.js
@@ -30,6 +30,43 @@ for (const [path, obj] of Object.entries(openapi.paths)) {
 
 console.log("Tags added!");
 
+console.log("Configure security schemes");
+
+if (!openapi?.components?.securitySchemes) {
+  openapi.components.securitySchemes = {
+    authToken: {
+      type: "apiKey",
+      in: "header",
+      name: "Auth-Token"
+    },
+    appId: {
+      type: "apiKey",
+      in: "header",
+      name: "App-ID"
+    }
+  };
+
+  openapi.security = [
+    {
+      authToken: [],
+      appId: []
+    }
+  ];
+
+  for (const [path, obj] of Object.entries(openapi.paths)) {
+    for (const method of Object.values(obj)) {
+      for (const parameter of method.parameters) {
+        if (parameter.in !== "header") continue;
+
+        if (parameter.name === "App-ID" || parameter.name === "Auth-Token")
+          parameter.required = false;
+      }
+    }
+  }
+}
+
+console.log("Security schemes configured!");
+
 fs.writeFileSync("./openapi.json", JSON.stringify(openapi, null, 2));
 
 console.log("Specs merged!");
